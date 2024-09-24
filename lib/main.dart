9229133ef07cd7d3d90c50d7dad9ff45d2d312f6
najmulmyself <najmul.myself@gmail.com>
2024-09-25 02:14:05 +0600

    signup ui done

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: AppTheme.darkMode,
       title: 'Material App',
       home: const SignupPage(),
